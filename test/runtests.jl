3538b0a8d5e8a52455f3b2a91b7c58ccc3c06de6
YuriMiyamori <miyamori.fluid.osaka@gmail.com>
2023-05-06 23:51:37 +0900

    rename

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ PORT = 8000
 @testset "Surrealdb.jl" begin
     # Surreal
     db = Surreal("ws://localhost:$PORT")
-    @test db.client_state == SurrealDB.ConnectionState(0)
+    @test db.client_state == Surrealdb.ConnectionState(0)
 
     #connect
     connect(db)
-    @test db.client_state == SurrealDB.ConnectionState(1)
+    @test db.client_state == Surrealdb.ConnectionState(1)
 
     # signin
     res = signin(db, user="root", pass="root")
@@ -51,5 +51,5 @@ PORT = 8000
     #ping
     @test ping(db)===nothing
     #close
-    @test close(db)==SurrealDB.ConnectionState(2)
+    @test close(db)==Surrealdb.ConnectionState(2)
 end
