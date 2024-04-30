08fb509538bf4aa7c55293666199ba648dbc36c1
YuriMiyamori <miyamori.fluid.osaka@gmail.com>
2024-04-30 13:10:47 +0000

    Surreal.jlの変更をコミット

--- src/surreal.jl
+++ src/surreal.jl
@@ -1,20 +1,5 @@
 @enum ConnectionState CONNECTING=0 CONNECTED=1 DISCONNECTED=2
-"""
-	Surreal(url::Union{Nothing, String}, token::Union{Nothing, String}, client_state::ConnectionState, ws::Union{Nothing, websocket}
-A struct represents a Surreal server.
-# Constructors
-```julia 
-Surreal()
-Surreal(url::String)
-```
-# Keyword arguments
-- url: The URL of the Surreal server.
-# Examples
-```jldoctest
-db = Surreal("ws://127.0.0.1:8000/rpc")
-db = Surreal("http://cloud.surrealdb.com/rpc")
-```
-"""
+
 mutable struct Surreal
 	url::String
 	token::Union{Nothing, String}
@@ -26,10 +11,11 @@ end
 """
 	Surreal(url::String; npool=1)::Surreal
 
-	A struct represents a Surreal server.
+	Retuen struct represents a Surreal server.
 	# Constructors
 	```julia 
 	Surreal(url::String)
+	Surreal(url::String, npool=4)
 	```
 	# Keyword arguments
 	- url: The URL of the Surreal server.
